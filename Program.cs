d604521b24acf598fa6050e03768b065fe36416b
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 15:27:25 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum");
+        }
+    }
+}
