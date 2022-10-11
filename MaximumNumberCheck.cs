c26a0fb88dbbecf1eb487746741264b0d8a1f448
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 15:45:56 +0530

    Finding Maximum Float Number

--- MaximumNumberCheck.cs
+++ MaximumNumberCheck.cs
@@ -8,6 +8,7 @@ namespace FindMaximumNumberProject
 {
     public class MaximumNumberCheck
     {
+        //Maximum Number for Intiger
         public static int MaximumIntigerNumber(int firstValue,int SecondValue,int ThirdValue)
         {
             if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(ThirdValue)>0)
@@ -19,6 +20,23 @@ namespace FindMaximumNumberProject
                 return SecondValue;
             }
             if(ThirdValue.CompareTo(firstValue)>0 && ThirdValue.CompareTo(SecondValue)>0)
+            {
+                return ThirdValue;
+            }
+            return firstValue;
+        }
+        //Maximum Number for Float
+        public static double MaximumFloatNumber(double firstValue, double SecondValue, double ThirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(ThirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (SecondValue.CompareTo(firstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
+            {
+                return SecondValue;
+            }
+            if (ThirdValue.CompareTo(firstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
             {
                 return ThirdValue;
             }
