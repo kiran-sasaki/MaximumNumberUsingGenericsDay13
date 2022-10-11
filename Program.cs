8b9712773009e3d9b657303564a27e845590bb6e
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-11 15:47:28 +0530

    Finding Maximum Float Number

--- Program.cs
+++ Program.cs
@@ -4,16 +4,25 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Find Maximum");
+            Console.WriteLine("Find Maximum number");
             //IntigerTestCase1
-            int IntigerTestCase1 = MaximumNumberCheck.MaximumIntigerNumber(23, 4, 5);
-            Console.WriteLine("Maximum no is " + IntigerTestCase1);
-            //IntigerTestCase2
-            int IntigerTestCase2 = MaximumNumberCheck.MaximumIntigerNumber(23, 45, 5);
-            Console.WriteLine("Maximum no is " + IntigerTestCase2);
-            //IntigerTestCase3
-            int IntigerTestCase3 = MaximumNumberCheck.MaximumIntigerNumber(23, 45, 54);
-            Console.WriteLine("Maximum no is " + IntigerTestCase3);
+            //int IntigerTestCase1 = MaximumNumberCheck.MaximumIntigerNumber(23, 4, 5);
+            //Console.WriteLine("Maximum no is " + IntigerTestCase1);
+            ////IntigerTestCase2
+            //int IntigerTestCase2 = MaximumNumberCheck.MaximumIntigerNumber(23, 45, 5);
+            //Console.WriteLine("Maximum no is " + IntigerTestCase2);
+            ////IntigerTestCase3
+            //int IntigerTestCase3 = MaximumNumberCheck.MaximumIntigerNumber(23, 45, 54);
+            //Console.WriteLine("Maximum no is " + IntigerTestCase3);
+            //FloatTestCase1
+            double FloatTestCase1 = MaximumNumberCheck.MaximumFloatNumber(45.6, 4.3, 5.7);
+            Console.WriteLine("Maximum no is " + FloatTestCase1);
+            //FloatTestCase2
+            double FloatTestCase2 = MaximumNumberCheck.MaximumFloatNumber(23.8, 42.8, 16.4);
+            Console.WriteLine("Maximum no is " + FloatTestCase2);
+            //FloatTestCase3
+            double FloatTestCase3 = MaximumNumberCheck.MaximumFloatNumber(32.7, 43.6, 76.8);
+            Console.WriteLine("Maximum no is " + FloatTestCase3);
         }
     }
 }
