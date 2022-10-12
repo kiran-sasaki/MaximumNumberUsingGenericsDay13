378c574c09cc942732bfa3edb696ef5325f11205
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-12 16:41:46 +0530

    Added Array Generics Method

---
+++ MaximumGenerisUsingArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumberProject
+{
+    public class MaximumGenericsUsingArray<T> where T : IComparable
+    {
+        public T[] value;
+
+        public MaximumGenericsUsingArray(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+
+        public T MaxMethod()
+        {
+            var Max = MaxValue(value);
+            return Max;
+        }
+    }
+}
