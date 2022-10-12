addc367a023af2a5d998b69edcef39ca0f1f992e
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-12 16:04:27 +0530

    Added Generic Class

--- Program.cs
+++ Program.cs
@@ -24,14 +24,35 @@
             //double FloatTestCase3 = MaximumNumberCheck.MaximumFloatNumber(32.7, 43.6, 76.8);
             //Console.WriteLine("Maximum no is " + FloatTestCase3);
             //StringTestCase1
-            string StringTestCase1 = MaximumNumberCheck.MaximumString("25", "15", "10");
-            Console.WriteLine("Maximum Number is " + StringTestCase1);
-            //StringTestCase2
-            string StringTestCase2 = MaximumNumberCheck.MaximumString("25", "45", "30");
-            Console.WriteLine("Maximum Number is " + StringTestCase2);
-            //StringTestCase3
-            string StringTestCase3 = MaximumNumberCheck.MaximumString("25", "75", "90");
-            Console.WriteLine("Maximum Number is " + StringTestCase3);
+            //string StringTestCase1 = MaximumNumberCheck.MaximumString("25", "15", "10");
+            //Console.WriteLine("Maximum Number is " + StringTestCase1);
+            ////StringTestCase2
+            //string StringTestCase2 = MaximumNumberCheck.MaximumString("25", "45", "30");
+            //Console.WriteLine("Maximum Number is " + StringTestCase2);
+            ////StringTestCase3
+            //string StringTestCase3 = MaximumNumberCheck.MaximumString("25", "75", "90");
+            //Console.WriteLine("Maximum Number is " + StringTestCase3);
+
+            GenericMaximum<int> maximum = new GenericMaximum<int>(45, 67, 57);
+            Console.WriteLine("The maximum No is "+maximum.MaxValue());
+            GenericMaximum<double> doublemaximum = new GenericMaximum<double>(45.5, 76.9, 25.7);
+            Console.WriteLine("The maximum No is " + doublemaximum.MaxValue());
+            GenericMaximum<string> stringmaximum = new GenericMaximum<string>("Apple","Peach","Banana");
+            Console.WriteLine("The maximum No is " + stringmaximum.MaxValue());
+
+
+
+            //int[] intarray = { 41, 23, 63, 45,76 };
+            //GenericMaximum<int> generic = new GenericMaximum<int>(intarray);
+            //generic.PrintMaxValue();
+
+            //double[] doublearray = { 23.7, 36.8, 83.3, 15.5, 96.9 };
+            //GenericMaximum<double> genericDouble = new GenericMaximum<double>(doublearray);
+            //genericDouble.PrintMaxValue();
+
+            //string[] stringarray = { "237", "368", "833", "155", "969" };
+            //GenericMaximum<string> genericString = new GenericMaximum<string>(stringarray);
+            //genericString.PrintMaxValue();
         }
     }
 }
